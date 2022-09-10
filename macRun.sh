a55fb3f42ba02ccc072783a4cfc41abb7a88d0a5
logicam <49878084+emanrodriguez@users.noreply.github.com>
2022-09-10 14:10:01 -0700

    Delete macRun.sh

--- macRun.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$USER/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
-brew install --cask microsoft-office
-brew install --cask microsoft-auto-update
-brew install --cask adobe-creative-cloud
